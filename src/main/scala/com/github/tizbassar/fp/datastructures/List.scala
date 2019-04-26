6f30ca617caa42c737a1a7082fcb18d29c3be446
Tolegen Izbassar <t.izbassar@gmail.com>
2019-04-26 19:37:39 +0300

    Add some functions to List

--- src/main/scala/com/github/tizbassar/fp/datastructures/List.scala
+++ src/main/scala/com/github/tizbassar/fp/datastructures/List.scala
@@ -56,4 +56,41 @@ object List {
     case Nil => z
     case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
   }
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A])((b, a) => Cons(a, b))
+
+  def append[A](l1: List[A], l2: List[A]): List[A] =
+    foldRight(l1, l2)(Cons(_, _))
+
+  def concat[A](ll: List[List[A]]): List[A] =
+    foldRight(ll, Nil: List[A])(append)
+
+  def addOne(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((a, b) => Cons(a + 1, b))
+
+  def convert(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((a, b) => Cons(a.toString, b))
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, Nil: List[B])((a, b) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Nil => Nil
+    case Cons(h, t) if f(h) => Cons(h, filter(t)(f))
+    case Cons(_, t) => filter(t)(f)
+  }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    foldRight(as, Nil: List[B])((h, t) => append(f(h), t))
+
+  def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(a => if (f(a)) List(a) else Nil)
+
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = (as, bs) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) =>
+      Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
 }
