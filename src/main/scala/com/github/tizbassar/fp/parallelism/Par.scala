0307c9a9eeac9b095c6bc6efb7000e004c7c5562
Tolegen Izbassar <t.izbassar@gmail.com>
2019-05-07 23:58:34 +0300

    Add Par representation

--- src/main/scala/com/github/tizbassar/fp/parallelism/Par.scala
+++ src/main/scala/com/github/tizbassar/fp/parallelism/Par.scala
@@ -1,14 +1,37 @@
 package com.github.tizbassar.fp.parallelism
-
-trait Par[A]
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
 
 object Par {
-  def run[A](a: Par[A]): A = ???
 
-  def map2[A, B, C](pa: => Par[A], pb: => Par[B])(f: (A, B) => C): Par[C] = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def fork[A](a: => Par[A]): Par[A] = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def unit[A](a: A): Par[A] = ???
+  def map2[A, B, C](pa: => Par[A], pb: => Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val af = pa(es)
+      val bf = pb(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A]{
+      def call = a(es).get
+    })
+
+  def unit[A](a: A): Par[A] = es => UnitFuture(a)
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled(): Boolean = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
 }
