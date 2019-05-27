a25218eac4522c1ea5f620662cf490d210d66e70
Tolegen Izbassar <t.izbassar@gmail.com>
2019-05-27 15:43:19 +0300

    Add flatMap as primitive combinator

--- src/main/scala/com/github/tizbassar/fp/parsing/Parsers.scala
+++ src/main/scala/com/github/tizbassar/fp/parsing/Parsers.scala
@@ -1,5 +1,6 @@
 package com.github.tizbassar.fp.parsing
 import com.github.tizbassar.fp.testing.Prop
+import scala.util.matching.Regex
 
 trait Parsers[ParseError, Parser[+ _]] { self =>
 
@@ -11,19 +12,39 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
   def char(c: Char): Parser[Char] =
     string(c.toString) map (_.charAt(0))
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
-  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
+    if (n <= 0) succeed(List())
+    else map2(p, listOfN(n - 1, p))(_ :: _)
 
-  def many[A](p: Parser[A]): Parser[List[A]]
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    or(map2(p, many(p))(_ :: _), succeed(List()))
 
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    flatMap(p) { a =>
+      map(p2)(b => (a, b))
+    }
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+    flatMap(p)(a => succeed(f(a)))
+
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+    flatMap(p)(a => map(p2)(b => f(a, b)))
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(
       implicit f: A => Parser[String]
   ): ParserOps[String] = ParserOps(f(a))
+  implicit def regex(r: Regex): Parser[String]
 
   val numA: Parser[Int] = char('a').many.map(_.size)
 
@@ -31,7 +52,11 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
     def many: Parser[List[A]] = self.many(p)
+    def slice: Parser[String] = self.slice(p)
+    def product[B](p2: Parser[B]) = self.product(p, p2)
+    def **[B](p2: Parser[B]) = self.product(p, p2)
   }
 
   object Laws {
