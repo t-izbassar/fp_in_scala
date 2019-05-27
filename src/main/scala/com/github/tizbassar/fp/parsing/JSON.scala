71662eb127cc576823831488fd6c8bbdbe40873a
Tolegen Izbassar <t.izbassar@gmail.com>
2019-05-27 15:49:47 +0300

    Add JSON.scala

---
+++ src/main/scala/com/github/tizbassar/fp/parsing/JSON.scala
@@ -0,0 +1,11 @@
+package com.github.tizbassar.fp.parsing
+
+trait JSON
+object JSON {
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+}
